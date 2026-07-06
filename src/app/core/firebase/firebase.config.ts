6bbdc516ebd7a12a6083fed1200e25c36641de84
jhoanjimz2 <jhoan.developer@gmail.com>
2026-07-06 06:37:55 -0500

    docs: add project documentation and delivery guide

--- src/app/core/firebase/firebase.config.ts
+++ src/app/core/firebase/firebase.config.ts
@@ -1,10 +1,10 @@
 import { FirebaseOptions } from '@angular/fire/app';
 
 export const firebaseConfig: FirebaseOptions = {
-  apiKey: 'AIzaSyDOBFBDOi3aaxYrSXNrJ2ORxJmtxpyETEA',
-  authDomain: 'accenture-todo-edf2b.firebaseapp.com',
-  projectId: 'accenture-todo-edf2b',
-  storageBucket: 'accenture-todo-edf2b.firebasestorage.app',
-  messagingSenderId: '1018721159520',
-  appId: '1:1018721159520:web:99ea8932aa92493459b605',
+  apiKey: '...',
+  authDomain: '...',
+  projectId: '...',
+  storageBucket: '...',
+  messagingSenderId: '...',
+  appId: '...',
 };
